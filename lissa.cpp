e356d757cc4d520ffb8b1f3f84f3d09f6a7ea672
boochow <boochowp@gmail.com>
2020-04-10 13:43:12 +0900

    refactor: delete unnecessary lines

--- lissa.cpp
+++ lissa.cpp
@@ -24,18 +24,14 @@ void DELFX_PROCESS(float *xn, uint32_t frames)
   float * __restrict x = xn;
   const float * x_e = x + 2*frames;
 
-//  const float p = s_param;
-  float p_z = s_param_z;
-  
   for (; x != x_e; ) {
     s_lfo.cycle();
     
     float wave = amp * s_lfo.sine_bi();
     
-    *(x++); // leave left channel un-delayed for comparitive earing
+    *(x++);
     *(x++) = wave;
   }
-  s_param_z = p_z;
 }
 
 
@@ -44,7 +40,6 @@ void DELFX_PARAM(uint8_t index, int32_t value)
   const float valf = q31_to_f32(value);
   switch (index) {
   case k_user_delfx_param_time:
-//    s_lfo.setF0(20.f + 420.f * valf, s_fs_recip);
       s_lfo.setF0(osc_notehzf(valf * 128), s_fs_recip);
     break;
   case k_user_delfx_param_depth:
