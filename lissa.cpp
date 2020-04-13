bd29c188dd36a0b1809226597648631a2c9c1c87
boochow <boochowp@gmail.com>
2020-04-13 23:34:56 +0900

    add comments, use #define for initial value of frequency

--- lissa.cpp
+++ lissa.cpp
@@ -14,6 +14,7 @@
 // longest delay is wave length/4 = 1/(8.2 * 4).
 // so 1/32 sec is sufficient for delay buffer.
 #define BUFSIZE 1500
+#define DEFAULTFREQ 220.f
 static dsp::DelayLine s_delay;
 
 static __sdram float s_delay_ram[BUFSIZE];
@@ -29,9 +30,9 @@ static int32_t lfo_type = 0;
 void DELFX_INIT(uint32_t platform, uint32_t api)
 {
   s_delay.setMemory(s_delay_ram, BUFSIZE);  
-  s_len = s_len_z = 48000.f / (4 * 220);
+  s_len = s_len_z = 48000.f / (4 * DEFAULTFREQ);
   s_lfo.reset();
-  s_lfo.setF0(220.f,s_fs_recip);
+  s_lfo.setF0(DEFAULTFREQ, s_fs_recip);
   amp = 1.0;
   offset = 0;
   lfo_type = 0;
@@ -83,17 +84,17 @@ void DELFX_PARAM(uint8_t index, int32_t value)
   case k_user_delfx_param_time:
       hz = osc_notehzf(valf * 128);
       s_lfo.setF0(hz, s_fs_recip);
-      s_len = 48000.f / (4 * hz);
+      s_len = 48000.f / (4 * hz);      /* delay length = 1/4 * wave length */
     break;
   case k_user_delfx_param_depth:
       amp = valf;
     break;
   case k_user_delfx_param_shift_depth:
       if (valf <= 0.4) {
-          offset = valf * 2.5 - 0.5;
+          offset = valf * 2.5 - 0.5;   /* offset = -0.5 .. 0.5 */
           lfo_type = 0;
       } else if (valf >= 0.6) {
-          offset = (1.0 - valf) * 2.5;
+          offset = (1.0 - valf) * 2.5; /* offset = 0.0 .. 1.0 */
           lfo_type = 1;
       } else {
           lfo_type = 2;
